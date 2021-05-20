7457ef69c2f3382a566ca762ebc6e4ff95c4ebe3
Ajay Kharat <iamajaykharat@gmail.com>
2021-05-20 15:53:15 +0530

    Optimised and ready to deploy

--- resources/js/script.js
+++ resources/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
       $('nav').removeClass('sticky');
     }
   }, {
-    offset: '60px;'
+    offset: '60px'
   });
 
   // Bannner Buttons Scrolling 
@@ -22,7 +22,7 @@ $(document).ready(function () {
 
   // Navigation Buttons Scrolling
   $('a[href*="#"]')
-  // Select all links with hashes
+    // Select all links with hashes
     // Remove links that don't actually link to anything
     .not('[href="#"]')
     .not('[href="#0"]')
@@ -58,9 +58,51 @@ $(document).ready(function () {
       }
     });
 
-    // Animation On Scrolling
-    
+  // Animation On Scrolling
 
+  $('.js--wp-1').waypoint(function (direction) {
+    $('.js--wp-1').addClass('animated fadeIn')
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-2').waypoint(function (direction) {
+    $('.js--wp-2').addClass('animated fadeInUp')
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-3').waypoint(function (direction) {
+    $('.js--wp-3').addClass('animated fadeIn')
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-4').waypoint(function (direction) {
+    $('.js--wp-4').addClass('animated pulse')
+  }, {
+    offset: '50%'
+  });
+
+  // Mobile Navigation
+
+  $('.js--nav-icon').click(function () {
+
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
+
+
+  });
 
 });
 
